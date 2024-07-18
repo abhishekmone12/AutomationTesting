e9d4dc1b41997632f7077dc4fb1343e9c8f894c6
Abhishek9-coder <abhishekmone09@gmail.com>
2024-07-18 17:58:58 +0530

    Committing StringReverse to BranchA

--- JavaPgms/src/Strings/ReverseString.java
+++ JavaPgms/src/Strings/ReverseString.java
@@ -2,9 +2,20 @@ package Strings;
 
 public class ReverseString {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+      
+		String s1 = "JAVA";
+		String s2 = "";
+		
+		for(int i = s1.length()-1;i>=0 ;i--)
+		{
+			
+			s2 = s2 + s1.charAt(i);
+		}
+		
+	
+		System.out.println(s2);
 	}
 
 }
