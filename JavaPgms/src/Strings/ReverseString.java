49db507f19eb9b83d5c06fca3ceedb283d154070
Abhishek9-coder <abhishekmone09@gmail.com>
2024-07-19 14:27:10 +0530

    Added additional checks...

--- JavaPgms/src/Strings/ReverseString.java
+++ JavaPgms/src/Strings/ReverseString.java
@@ -1,21 +1,24 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class ReverseString {
 
 	public static void main(String[] args)
 	{
-      
-		String s1 = "JAVA";
-		String s2 = "";
-		
-		for(int i = s1.length()-1;i>=0 ;i--)
-		{
-			
-			s2 = s2 + s1.charAt(i);
-		}
-		
-	
-		System.out.println(s2);
+     String reversedStr = "";
+	 Scanner sc =new Scanner(System.in);
+	 
+	 System.out.println("Enter a String");
+	  String orginalStr = sc.nextLine();
+	  
+	  for(int i = orginalStr.length()-1 ;i >= 0 ; i--)
+	  {
+		  
+		  reversedStr = reversedStr + orginalStr.charAt(i);
+;	  }
+	  System.out.println("Rversed String is :" +reversedStr);
+	    
 	}
 
 }
